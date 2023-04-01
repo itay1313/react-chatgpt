a6135cbb6a833119e219ac1675afc729af5d9b8e
itay haephrati <itay1313@gmail.com>
2023-04-01 10:49:52 +0300

    u

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-
+require("dotenv").config()
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-// Creatre a simple express api that calls the function above
-
-
 const app = express();
+app.use(bodyParser.json());
+app.use(cors())
 const port = 3080;
 
 app.post("/", async (req, res) => {
+  const { message } = req.body;
+
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
   });
-  console.log(response.data.choices[0].text)
   res.json({
-    data: response.data
+    message: response.data.choices[0].text,
   })
 });
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './normal.css';
 import './App.css';
 
@@ -9,34 +9,49 @@ function App() {
   const [chatLog, setChatLog] = useState([{
     user: "gpt",
     message: "How can I help you?"
+  },
+  {
+    user: "me",
+    message: "I want to buy a new phone"
   }]);
+
+  //clear chats
+  function clearChat() {
+    setChatLog([]);
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setChatLog([...chatLog, { user: "me", message: `${input}` }])
-    setInput("");
+    await setChatLog([...chatLog, { user: "me", message: `${input}` }])
+    await setInput("");
+    const message = chatLog.map((message) => message.message).join("\n");
+    const response = await fetch('http://localhost:3080/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: message })
+
+    });
+    const data = await response.json();
+    await setChatLog([...chatLog, { user: "gpt", message: `${data.message}` }])
   }
 
   return (
     <div className='App'>
       <aside className='sidemenu box-component'>
-        <button class="btn btn-hover">
-          <span class="btn-text">New Chat</span>
+        <button className="btn btn-hover" onClick={clearChat}>
+          <span className="btn-text">New Chat</span>
         </button>
       </aside>
-      <main className='chatbox'>
-        <header class="flex fullwidth justify-between">
-          <div class="logo">Heyi</div>
+      <main className='chatbox flex-col'>
+        <header className="flex fullwidth justify-between">
+          <div className="logo">Heyi</div>
         </header>
-        <h1>Main</h1>
         <div className="chat-log">
           {chatLog.map((message, index) => (
             <ChatMessage key={index} message={message} />
           ))}
-          <div className="chat-message chatgpt">
-            <div className="avatar"><img class="avatar" alt="profile" src="https://res.cloudinary.com/damqsjjsn/image/upload/v1641745444/Mask_Group_1_ifmexq.png" /></div>
-            <div className="message">Hello world</div>
-          </div>
         </div>
         <div className="chat-input-holder">
           <form onSubmit={handleSubmit}>
@@ -58,7 +73,9 @@ function App() {
 const ChatMessage = ({ message }) => {
   return (
     <div className="chat-message">
-      <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}><img class="avatar" alt="profile" src="https://res.cloudinary.com/damqsjjsn/image/upload/v1641745444/Mask_Group_1_ifmexq.png" /></div>
+      <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}>
+        <img className="avatar" alt="profile" src=
+          {message.user === "gpt" ? 'https://res.cloudinary.com/damqsjjsn/image/upload/v1641745444/Mask_Group_1_ifmexq.png' : 'https://res.cloudinary.com/damqsjjsn/image/upload/v1641672156/samples/people/boy-snow-hoodie.jpg'} /></div>
       <div className="message">{message.message}</div>
     </div>
   )
